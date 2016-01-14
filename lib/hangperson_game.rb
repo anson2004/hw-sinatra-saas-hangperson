bbf52463a162337e926e8e26e853b04041e47ac7
Yiqing Liang <yiqing.liang@hotmail.com>
2016-01-14 21:08:36 +0000

    setup

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -4,12 +4,15 @@ class HangpersonGame
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
+  attr_accessor :word, :guesses, :wrong_guesses 
   # def initialize()
   # end
   
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -19,4 +22,33 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  def guess letter
+    throw ArgumentError if letter.nil? || letter.length != 1
+    throw ArgumentError if not letter.match(/[A-Za-z]/)
+    letter.downcase!
+    if @word.include? letter
+      if not @guesses.include? letter
+        @guesses << letter
+        return true
+      end
+    else
+      if not @wrong_guesses.include? letter
+        @wrong_guesses << letter
+        return true
+      end
+    end
+    false
+  end
+  
+  def word_with_guesses
+    res = ''
+    @word.each_char{ |c| if not @guesses.include? c then  res << '-' else res << c end}
+    res
+  end
+  
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    @word.each_char{ |c| if not @guesses.include? c then return :play end }
+    :win
+  end
 end
